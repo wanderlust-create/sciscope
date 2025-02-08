63f55729ea326d104fdc18cb742c400fcf129dfe
Tamara Dowis <tamara.dowis@gmail.com>
2025-02-07 21:55:52 -0600

    fix: move dotenv loading to separate config file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("./src/config/loadEnv");
 
 const PORT = process.env.PORT || 3000;
 
---
+++ src/config/loadEnv.js
@@ -0,0 +1,3 @@
+require("dotenv").config({
+  path: require("path").resolve(__dirname, "../../.env"),
+});
