7cb53f835b8a717b4460dddd5fc7a33299afc996
Tamara Dowis <tamara.dowis@gmail.com>
2025-02-13 20:32:20 -0600

    feat: implement news controller with science news fetching functionality

---
+++ src/controllers/newsController.js
@@ -0,0 +1,12 @@
+import NewsService from "../services/newsService.js";
+
+const getNews = async (req, res) => {
+  try {
+    const news = await NewsService.fetchScienceNews();
+    res.status(200).json(news);
+  } catch {
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+};
+
+export default { getNews };
