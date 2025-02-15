7f5893410a5a4358d5cc963fa34f482134e48929
Tamara Dowis <tamara.dowis@gmail.com>
2025-02-15 17:17:30 -0600

    feat: add auth routes to API router

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import newsRoutes from "./newsRoutes.js";
+import authRoutes from "./authRoutes.js";
 
 const router = express.Router();
 
 router.use("/news", newsRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
