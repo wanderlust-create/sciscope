7314fa121f9ef5906e6d05efc7ef421a7b568fa8
Tamara Dowis <tamara.dowis@gmail.com>
2025-02-16 00:34:14 -0600

    feat: add protected routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import newsRoutes from './newsRoutes.js';
 import authRoutes from './authRoutes.js';
+import protectedRoutes from './protectedRoutes.js';
 
 const router = express.Router();
 
 router.use('/news', newsRoutes);
 router.use('/auth', authRoutes);
+router.use(protectedRoutes);
 
 export default router;
