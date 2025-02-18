336806c099f97660b28f4fac697aa1bdc7aebeb1
Tamara Dowis <tamara.dowis@gmail.com>
2025-02-17 19:34:22 -0600

    feat: Add database cleanup in setupTests before all tests

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,7 +1,16 @@
 import dotenv from 'dotenv';
+import db from './../src/config/db.js';
+import logger from './../src/loaders/logger.js';
 
 dotenv.config();
 
 import { jest } from '@jest/globals';
+
+beforeAll(async () => {
+  logger.info('🧹 Clearing test data...');
+  await db('articles').del();
+  await db('user_bookmarks').del();
+  await db('bookmark_group_assignments').del();
+});
 
 jest.setTimeout(30000);
