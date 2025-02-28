3c2b88321e4c224c804fbbfd11755326ee088233
Tamara Dowis <tamara.dowis@gmail.com>
2025-02-27 20:24:56 -0600

    feat: add search route for articles in API

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import newsRoutes from './newsRoutes.js';
 import authRoutes from './authRoutes.js';
 import protectedRoutes from './protectedRoutes.js';
+import searchArticles from './articles.search.js';
 
 const router = express.Router();
 
 router.use('/news', newsRoutes);
 router.use('/auth', authRoutes);
 router.use(protectedRoutes);
+router.use('/search', searchArticles);
 
 export default router;
