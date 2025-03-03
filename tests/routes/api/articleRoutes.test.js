f1d1d0d9092a523e0af1dde13aff9f3de87dd41c
Tamara Dowis <tamara.dowis@gmail.com>
2025-03-02 22:05:05 -0600

    test: update article routes tests to use queryService for article search

--- tests/routes/api/articleRoutes.test.js
+++ tests/routes/api/articleRoutes.test.js
@@ -2,9 +2,9 @@ import { jest } from '@jest/globals';
 import request from 'supertest';
 import db from '../../../src/config/db.js';
 import createServer from '../../../src/loaders/server.js';
-import { default as articleSearchService } from '../../../src/services/articleSearchService.js';
+import { default as queryService } from '../../../src/services/queryService.js';
 import { generateMockSavedArticles } from '../../mocks/generateMockSavedArticles.js';
-const searchArticles = jest.spyOn(articleSearchService, 'searchArticles');
+const processQueryRequest = jest.spyOn(queryService, 'processQueryRequest');
 
 const app = createServer();
 let server;
@@ -22,17 +22,13 @@ afterAll(async () => {
 });
 
 describe('Articles Controller', () => {
-  describe('GET /api/v1/articles/search', () => {
+  describe('GET /api/v1/articles', () => {
     it('should return 200 OK and a list of articles matching the query', async () => {
       const mockedArticles = generateMockSavedArticles(6, 'mars');
 
-      searchArticles.mockResolvedValue(mockedArticles);
+      processQueryRequest.mockResolvedValue(mockedArticles);
 
-      const response = await request(app).get(
-        '/api/v1/articles/search?query=mars'
-      );
-      console.log('Response:', response.body);
-
+      const response = await request(app).get('/api/v1/articles?query=mars');
       expect(response.status).toBe(200);
       expect(Array.isArray(response.body)).toBe(true);
       expect(response.body.length).toBeGreaterThan(0);
@@ -43,17 +39,15 @@ describe('Articles Controller', () => {
       expect(article).toHaveProperty('url');
       expect(article).toHaveProperty('publishedAt');
 
-      // Ensure searchArticles was called exactly once
-      expect(searchArticles).toHaveBeenCalledTimes(1);
-      expect(searchArticles).toHaveBeenCalledWith('mars');
+      // Ensure processQueryRequest was called exactly once
+      expect(processQueryRequest).toHaveBeenCalledTimes(1);
+      expect(processQueryRequest).toHaveBeenCalledWith('mars');
     });
 
     it('should return 500 Internal Server Error when the search fails', async () => {
-      searchArticles.mockRejectedValueOnce(new Error('Search Error'));
+      processQueryRequest.mockRejectedValueOnce(new Error('Search Error'));
 
-      const response = await request(app).get(
-        '/api/v1/articles/search?query=mars'
-      );
+      const response = await request(app).get('/api/v1/articles?query=mars');
       expect(response.status).toBe(500);
       expect(response.body).toStrictEqual({ error: 'Internal Server Error' });
     });
