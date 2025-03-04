420decbae3a4c2932268135d419b5922c99bf559
Tamara Dowis <tamara.dowis@gmail.com>
2025-03-03 21:22:09 -0600

    feat: add Article model with JSON schema and relation mappings

---
+++ src/models/Article.js
@@ -0,0 +1,44 @@
+import { Model } from 'objection';
+import db from '../config/db.js';
+
+Model.knex(db);
+
+class Article extends Model {
+  static get tableName() {
+    return 'articles';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['title', 'url'],
+      properties: {
+        id: { type: 'integer' },
+        title: { type: 'string', minLength: 1, maxLength: 255 },
+        description: { type: ['string', 'null'] },
+        url: { type: 'string', format: 'uri' },
+        url_to_image: { type: ['string', 'null'], format: 'uri' },
+        published_at: { type: ['string', 'null'], format: 'date-time' },
+        author_name: { type: ['string', 'null'] },
+        source_name: { type: ['string', 'null'] },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    const Bookmark = require('./Bookmark');
+
+    return {
+      bookmarks: {
+        relation: Model.HasManyRelation,
+        modelClass: Bookmark,
+        join: {
+          from: 'articles.id',
+          to: 'user_bookmarks.article_id',
+        },
+      },
+    };
+  }
+}
+
+export default Article;
