0a0420336350b12da821538e9c652d61cbf945c6
Tamara Dowis <tamara.dowis@gmail.com>
2025-03-20 17:20:30 -0500

    chore: debugging GitHub Actions

--- tests/integration/routes/api/newsPagination.test.js
+++ tests/integration/routes/api/newsPagination.test.js
@@ -41,6 +41,7 @@ describe('GET /api/v1/news Pagination', () => {
     await storeArticlesInDB(mockRecentArticles);
 
     const res = await request(app).get('/api/v1/news?page=1&limit=5');
+    console.log('test 1 res', res.body);
 
     expect(res.status).toBe(200);
     expect(res.body.total_count).toBe(15); // Mocked total count
@@ -61,7 +62,7 @@ describe('GET /api/v1/news Pagination', () => {
     await storeArticlesInDB(mockRecentArticles);
 
     const res = await request(app).get('/api/v1/news?page=2&limit=5');
-
+    console.log('test 2 res', res.body);
     expect(res.status).toBe(200);
     expect(res.body.current_page).toBe(2);
     expect(res.body.articles.length).toBe(5);
@@ -79,6 +80,7 @@ describe('GET /api/v1/news Pagination', () => {
     await storeArticlesInDB(mockRecentArticles);
 
     const res = await request(app).get('/api/v1/news?page=999&limit=5');
+    console.log('test 3 res', res.body);
 
     expect(res.status).toBe(200);
     expect(res.body.articles).toEqual([]);
@@ -97,6 +99,7 @@ describe('GET /api/v1/news Pagination', () => {
     await storeArticlesInDB(mockRecentArticles);
 
     const res = await request(app).get('/api/v1/news');
+    console.log('test 4 res', res.body);
 
     expect(res.status).toBe(200);
     expect(res.body.current_page).toBe(1);
@@ -116,6 +119,7 @@ describe('GET /api/v1/news Pagination', () => {
     await storeArticlesInDB(mockRecentArticles);
 
     const res = await request(app).get('/api/v1/news?page=1&limit=5');
+    console.log('test 5 res', res.body);
 
     expect(res.status).toBe(200);
     const { articles } = res.body;
