43ba784c3bba5bbe7b9579a20bbf1370e042a7dd
Tamara Dowis <tamara.dowis@gmail.com>
2025-04-20 13:46:43 -0500

    feat: protect routes with validateNumericParam middleware

--- src/routes/api/bookmarkRoutes.js
+++ src/routes/api/bookmarkRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import bookmarkController from '../../controllers/bookmarksController.js';
-import authMiddleware from '../../middleware/authMiddleware.js';
+import { authMiddleware, validateNumericParam } from '../../middleware';
 
 const router = express.Router();
 
 router.get('/', authMiddleware, bookmarkController.getBookmarks);
 router.post('/', authMiddleware, bookmarkController.createBookmark);
-router.delete('/:id', authMiddleware, bookmarkController.deleteBookmark);
+router.delete(
+  '/:id',
+  authMiddleware,
+  validateNumericParam('id'),
+  bookmarkController.deleteBookmark
+);
 
 export default router;
