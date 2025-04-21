e56e3f42db7f6acac49c36a17d8913039ec9f49d
Tamara Dowis <tamara.dowis@gmail.com>
2025-04-21 14:47:54 -0500

    fix: update dotenv configuration to load from default .env file

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -6,12 +6,13 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({
-  path: path.resolve(
-    __dirname,
-    `../../.env.${process.env.NODE_ENV || 'development'}`
-  ),
-});
+// dotenv.config({
+//   path: path.resolve(
+//     __dirname,
+//     `../../.env.${process.env.NODE_ENV || 'development'}`
+//   ),
+// });
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const config = {
   development: {
