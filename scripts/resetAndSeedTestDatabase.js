8bcc95edb41e2c7f7bbb5c3937f3738e8b7d8339
Tamara Dowis <tamara.dowis@gmail.com>
2025-04-25 16:11:49 -0500

    fix: update seedDatabase import and clean up logging in resetAndSeedDatabase

--- scripts/resetAndSeedTestDatabase.js
+++ scripts/resetAndSeedTestDatabase.js
@@ -1,27 +1,21 @@
 import logger from '../src/loaders/logger.js';
 import resetDatabase from './resetTestDatabase.js';
-import seedDatabase from './generateTestData.js';
+import seedDatabase from './seedTestDatabase.js';
 
 // Default to 'test' if NODE_ENV is not provided
 process.env.NODE_ENV = process.env.NODE_ENV || 'test';
-
 const env = process.env.NODE_ENV;
 
-logger.info(`🚀 Resetting and seeding the "${env}" database...`);
-
-// Run reset script
 async function resetAndSeedDatabase() {
   try {
     logger.info(`🚀 Resetting and seeding the "${env}" database...`);
     await resetDatabase();
-    logger.info(`✅ Database reset complete!`);
-    logger.info(`🚀 Seeding database with test data for "${env}"...`);
     await seedDatabase();
-    logger.info(`🎉 Database seeding complete!`);
-    process.exit(0); // <- clean exit if running standalone
+    process.exit(0);
   } catch (error) {
     console.error(`❌ Error during reset and seed: ${error.message}`);
     process.exit(1);
   }
 }
+
 resetAndSeedDatabase();
