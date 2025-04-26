4cb2419cb23cebc7de780a796897be3580ce30b3
Tamara Dowis <tamara.dowis@gmail.com>
2025-04-25 21:22:41 -0500

    test(articles): add sad path test for missing keyword in search route

--- tests/integration/routes/api/articleSearchRoutes.test.js
+++ tests/integration/routes/api/articleSearchRoutes.test.js
@@ -3,18 +3,19 @@ import request from 'supertest';
 import db from '../../../../src/config/db.js';
 import createServer from '../../../../src/loaders/server.js';
 import { default as queryService } from '../../../../src/services/queryService.js';
-import { generateMockSavedArticles } from '../../../mocks/generateMockSavedArticles.js';
+import { generateMockArticlesResponse } from '../../../mocks/generateMockArticles.js';
 const processQueryRequest = jest.spyOn(queryService, 'processQueryRequest');
 
 const app = createServer();
 let server;
 
 beforeAll(() => {
-  server = app.listen(8080);
+  server = app.listen(0);
 });
 
 beforeEach(async () => {
   await db('articles').del();
+  jest.clearAllMocks();
 });
 
 afterAll(async () => {
@@ -30,16 +31,24 @@ afterAll(async () => {
 describe('Articles Controller', () => {
   describe('GET /api/v1/search', () => {
     it('should return 200 OK and a list of articles matching the query', async () => {
-      const mockedArticles = generateMockSavedArticles(6, 'mars');
+      const mockRecentArticles = generateMockArticlesResponse(
+        6, // number of articles
+        'mars', // keyword
+        true, //IsNew
+        false, // IsOld
+        3 // max age hours
+      );
 
-      processQueryRequest.mockResolvedValue(mockedArticles);
+      processQueryRequest.mockResolvedValue(mockRecentArticles);
 
       const response = await request(app).get('/api/v1/search?keyword=mars');
       expect(response.status).toBe(200);
-      expect(Array.isArray(response.body)).toBe(true);
-      expect(response.body.length).toBeGreaterThan(0);
+      expect(Array.isArray(response.body.articles)).toBe(true);
+      expect(response.body.totalResults).toBe(6);
+      expect(response.body.articles.length).toBe(6);
+      expect(processQueryRequest).toHaveBeenCalledTimes(1);
 
-      const article = response.body[0];
+      const article = response.body.articles[0];
       expect(article).toHaveProperty('source.name');
       expect(article).toHaveProperty('title');
       expect(article).toHaveProperty('url');
@@ -48,6 +57,14 @@ describe('Articles Controller', () => {
       // Ensure processQueryRequest was called exactly once
       expect(processQueryRequest).toHaveBeenCalledTimes(1);
       expect(processQueryRequest).toHaveBeenCalledWith('mars', 1, 10);
+    });
+
+    it('should return 400 Bad Request when no keyword is provided', async () => {
+      const response = await request(app).get('/api/v1/search');
+      expect(response.status).toBe(400);
+      expect(response.body).toStrictEqual({
+        error: 'Keyword parameter is required.',
+      });
     });
 
     it('should return 500 Internal Server Error when the search fails', async () => {
