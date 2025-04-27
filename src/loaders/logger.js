c17b4b24e21a31c746e149732726c81f437b3754
Tamara Dowis <tamara.dowis@gmail.com>
2025-04-26 20:14:08 -0500

    refactor(logger): enhance logging format

--- src/loaders/logger.js
+++ src/loaders/logger.js
@@ -1,21 +1,28 @@
-import dotenv from 'dotenv';
-import winston from 'winston';
+import { createLogger, format, transports } from 'winston';
 
-dotenv.config();
+const { combine, timestamp, printf, colorize, errors, json } = format;
 
-const logger = winston.createLogger({
+const logger = createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: winston.format.json({ space: 4 }),
+  format: combine(timestamp(), errors({ stack: true }), json({ space: 2 })),
   transports: [
-    new winston.transports.File({ filename: 'logs/all.log' }),
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/all.log' }),
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
   ],
 });
 
+// Log to console during development
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
+    new transports.Console({
+      format: combine(
+        colorize(),
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        printf(({ timestamp, level, message, stack }) => {
+          const baseLog = `${timestamp} ${level}: ${message}`;
+          return stack ? `${baseLog}\n${stack}` : baseLog;
+        })
+      ),
     })
   );
 }
